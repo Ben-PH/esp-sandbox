cf3437d4aa8ce6e31a77e33b1877e96881e1bd80
Ben PHL <benphawke@gmail.com>
2023-06-25 00:25:03 +0200

    get it working!

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ fn main() -> ! {
 
     // We use one of the RMT channels to instantiate a `SmartLedsAdapter` which can
     // be used directly with all `smart_led` implementations
-    let mut led = <smartLedAdapter!(1)>::new(pulse.channel0, io.pins.gpio2);
+    let mut led = <smartLedAdapter!(2)>::new(pulse.channel0, io.pins.gpio7);
 
     // Initialize the Delay peripheral, and use it to toggle the LED state in a
     // loop.
@@ -58,21 +58,28 @@ fn main() -> ! {
         sat: 255,
         val: 255,
     };
+    let mut color2 = Hsv {
+        hue: 0,
+        sat: 255,
+        val: 255,
+    };
     let mut data;
 
     loop {
         // Iterate over the rainbow!
         for hue in 0..=255 {
             color.hue = hue;
+            color2.hue = 255 - hue;
+
             // Convert from the HSV color space (where we can easily transition from one
             // color to the other) to the RGB color space that we can then send to the LED
-            data = [hsv2rgb(color)];
+            data = [hsv2rgb(color), hsv2rgb(color2)];
             // When sending to the LED, we do a gamma correction first (see smart_leds
             // documentation for details) and then limit the brightness to 10 out of 255 so
             // that the output it's not too bright.
-            led.write(brightness(gamma(data.iter().cloned()), 10))
+            led.write(brightness(gamma(data.iter().cloned()), 60))
                 .unwrap();
-            delay.delay_ms(5u8);
+            delay.delay_ms(10u8);
         }
     }
 }
